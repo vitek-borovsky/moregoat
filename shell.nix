a1f080fcb98252cb31e8a7941cce8643f4ade76a
vitek-borovsky <vitek.borovsky@gmail.com>
2025-02-19 22:31:21 +0100

    Adding shell.nix

---
+++ shell.nix
@@ -0,0 +1,13 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    python311
+    (python311.withPackages (ps: with ps; [ flask ])) # Include Flask directly
+  ];
+
+  shellHook = ''
+    echo "Flask development environment activated!"
+  '';
+}
+
