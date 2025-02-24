97a536bd2beaeb3cee96902af515ce724229561a
vitek-borovsky <vitek.borovsky@gmail.com>
2025-02-24 10:34:35 +0100

    initial draft of protocols and HLC

---
+++ shell.nix
@@ -0,0 +1,21 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    pyright
+    (python311.withPackages (ps: with ps; [
+        flask
+        flask-socketio
+        simple-websocket
+        eventlet
+        pytest
+    ]))
+
+    nodejs_20
+    tailwindcss
+  ];
+
+  shellHook = ''
+    echo "Flask development environment activated!"
+  '';
+}
