545179903e0da978008c7044f680e0bc7f5e57cd
vitek-borovsky <vitek.borovsky@gmail.com>
2025-02-25 13:30:59 +0100

    board displayed in grid

--- frontend/src/Board.tsx
+++ frontend/src/Board.tsx
@@ -1,4 +1,5 @@
 import { useEffect } from 'react';
+import Squere from './Squere.tsx';
 
 interface BoardProps {
     boardSize: number;
@@ -8,9 +9,19 @@ const Board: React.FC<BoardProps> = ({ boardSize }) => {
   return (
     <>
         <p>This is board { boardSize }</p>
+        <div
+            style = {{
+                display: "grid",
+                gridTemplateColumns: `repeat(${boardSize}, minmax(0, 1fr))`,
+                gridTemplateRows: `repeat(${boardSize}, minmax(0, 1fr))`,
+            }}
+        >
+            { Array.from({ length: boardSize * boardSize}).map((_, index) => (
+                <Squere />
+            ))}
+        </div>
     </>
   );
 }
 
-export default Board
-
+export default Board;
---
+++ frontend/src/Squere.tsx
@@ -0,0 +1,15 @@
+interface SquereProps { }
+
+const Squere: React.FC<SquereProps> = ({}) => {
+    return (
+        <div
+        className="border border-gray-500 w-[20px] h-[20px]"
+        style={{
+            backgroundColor: "purple"
+        }}>
+        SSS
+        </div>
+    );
+}
+
+export default Squere
