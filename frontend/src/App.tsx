15c5bd2347f577d6f663736864896d11fed85b5f
vitek-borovsky <vitek.borovsky@gmail.com>
2025-02-27 11:49:15 +0100

    CreateGame - Board component switching

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,28 @@
-import { useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import WebSocketService from './WebSocketService.tsx'
 import Board from './components/Board/Board'
+import CreateGame from './components/CreateGame/CreateGame'
 import './App.css'
 
 function App() {
-  const wss = new WebSocketService()
-  return (
-    <>
+    const wss = useRef(null);
+    const [gameStarted, setGameStarted] = useState(false);
+    useEffect(() => {
+        wss.current = new WebSocketService()
+    }, [])
+
+    return (
+      <>
         <div>
-            <h1>Hello World</h1>
-            <button onClick={wss.sendEcho}>Send Echo</button>
+          <h1>Hello World</h1>
+          <button onClick={wss.sendEcho}>Send Echo</button>
         </div>
-        <Board boardSize={5} />
-    </>
-  );
+
+        { gameStarted || <CreateGame /> }
+        { gameStarted && <Board boardSize={5} /> }
+        <button onClick={() => setGameStarted(true)}>Start Game</button>
+      </>
+    );
 }
 
 export default App
---
+++ frontend/src/components/CreateGame/CreateGame.tsx
@@ -0,0 +1,11 @@
+
+interface CreateGameProps {
+}
+
+const CreateGame: React.FC<BoardProps> = ({ }) => {
+    return (
+        <h3>This is CreateGame component</h3>
+    );
+}
+
+export default CreateGame
