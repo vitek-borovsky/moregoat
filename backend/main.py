b0a2245ec53887a77dd7b5a5fdea969221657d6c
vitek-borovsky <vitek.borovsky@gmail.com>
2025-03-03 12:46:01 +0100

    cleaning code

--- backend/main.py
+++ backend/main.py
@@ -3,10 +3,10 @@ import eventlet
 eventlet.monkey_patch()
 
 from server import Server
-from flask import Flask
 
 PORT = 5000
 KEY_LENGTH = 3
 
 server = Server("MoreGoat")
-server.start_server(PORT, True)
+# server.start_server(PORT, True)
+server.start_server(PORT)
