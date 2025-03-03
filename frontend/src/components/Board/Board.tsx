270c8abe8acd0289b4e8f1239de3be7495e063dc
vitek-borovsky <vitek.borovsky@gmail.com>
2025-03-03 13:21:37 +0100

    rendering colors on click

--- frontend/src/components/Board/Board.tsx
+++ frontend/src/components/Board/Board.tsx
@@ -1,16 +1,26 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import Squere from './Squere';
 
 interface BoardProps {
     boardSize: number;
 }
 
+
 const Board: React.FC<BoardProps> = ({ boardSize }) => {
+    const [board, setBoard] = useState(Array(boardSize * boardSize).fill(-1));
+
     const handleClick = (index) => {
         const col = index % boardSize;
         const row = Math.floor(index / boardSize);
-        console.log(`clicked on (${col}, ${row})`);
+        setBoard((prevBoard) => {
+          const newBoard = [...prevBoard];
+            newBoard[index] += 1;
+            return newBoard;
+        });
+        console.log(`clicked on (${col}, ${row}), new value=${board[index]}`);
     }
+
+
 
     return (
       <div
@@ -21,7 +31,7 @@ const Board: React.FC<BoardProps> = ({ boardSize }) => {
           }}
       >
           { Array.from({ length: boardSize * boardSize}).map((_, index) => (
-              <Squere key={index} color = { "yellow" } onClick={ () => handleClick(index) } />
+              <Squere key={index} color = { board[index] } onClick={ () => handleClick(index) } />
           ))}
       </div>
     );
--- frontend/src/components/Board/Squere/Squere.tsx
+++ frontend/src/components/Board/Squere/Squere.tsx
@@ -1,15 +1,22 @@
 interface SquereProps {
-    color: string;
+    color: number;
     onClick: (number) => void;
 }
 
 const Squere: React.FC<SquereProps> = ({ color, onClick }) => {
+
+    const getColor = (value) => {
+        if  (value === -1) return "white";
+        const colors = [ "blue", "green", "orange", "pink", "purple" ]
+        return colors[value];
+    }
+
     return (
         <div
         className="grid border gorder-gray-500 w-[50px] h-[50px]"
         onClick={onClick}
         style={{
-            backgroundColor: "yellow"
+            backgroundColor: getColor(color)
         }}>
         </div>
     );
