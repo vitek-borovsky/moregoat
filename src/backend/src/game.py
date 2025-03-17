18860027942ebdac22ea079416c1f66193731760
vitek-borovsky <vitek.borovsky@gmail.com>
2025-03-17 14:37:52 +0100

    adding starting game

--- src/backend/src/game.py
+++ src/backend/src/game.py
@@ -6,31 +6,31 @@ class Game:
     def __init__(
             self, game_id: str, player_count: int, board_size: int
             ) -> None:
-        self.game_id = game_id
-        self.player_count = player_count
-        self.board_size = board_size
-        self.next_player_id = 0
-
-        # TODO remove this it's for testing only
-        self._start_game()
+        self._game_id = game_id
+        self._player_count = player_count
+        self._board_size = board_size
+        self._next_player_id = 0
 
     def __repr__(self) -> str:
         return f"""Game(
-            { self.game_id },
-            { self.player_count },
-            board_size={ self.board_size })"""
+            { self._game_id },
+            { self._player_count },
+            board_size={ self._board_size })"""
 
     def _increase_player_on_turn(self) -> None:
-        self.player_on_turn = (self.player_on_turn + 1) % self.player_count
+        self.player_on_turn = (self.player_on_turn + 1) % self._player_count
+
+    def try_start_game(self) -> bool:
+        return self._next_player_id == self._player_count
 
     def _start_game(self):
-        self.points = [0 for _ in range(self.player_count)]
-        self.board = Board(self.player_count, self.board_size)
+        self.points = [0 for _ in range(self._player_count)]
+        self.board = Board(self._player_count, self._board_size)
         self.player_on_turn = 0
 
     def request_player_id(self) -> int:
-        id = self.next_player_id
-        self.next_player_id += 1
+        id = self._next_player_id
+        self._next_player_id += 1
         return id
 
     def place_stone(
